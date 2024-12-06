4b45adf29d63f04b2ad8d61684b4b0483df60730
idowein <ido.weinstock@gmail.com>
2024-12-06 13:36:57 +0200

    adjusting / action

--- class_works/calculator_switch_based.c
+++ class_works/calculator_switch_based.c
@@ -1,43 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS
 # include <stdio.h>
-#include <math.h>
+#define _CRT_SECURE_NO_WARNINGS
 
-void main() {	
-	// This program implement calculator with basic math features based on if function 
+void main() {
 
-	printf("Welcome to my calculator!\n");
+	int a, b;
+	double result;
+	char op;
+	printf("Please enter one number\n");
+	scanf_s("%d", &a);
+	rewind(stdin);
+	printf("Please enter an operation\n");
+	scanf_s("%ch", &op);
+	printf("Please enter one number\n");
+	scanf_s("%d", &b);
 
-	double a, b, sum;
-	char math_sign;
+	switch (op) {
 
-	printf("please type one number\n");
-	scanf("%lf", &a);
-
-	rewind(stdin); // cleasr the input from keyboard
-	printf("please type math operation\n");
-	scanf("%ch", &math_sign);
-
-	printf("please type one number\n");
-	scanf("%lf", &b);
-
-	switch (math_sign) {
-
-	case '+': sum = a + b; 
-		printf("Result is %lf\n", sum);
+	case '+':
+		result = (double)(a + b); 
+		break;
+	case '-':
+		result = (double)(a - b);
+		break;
+	case '*':
+		result = (double)(a * b);
+		break;
+	case '/':
+		if (b != 0) {
+			result = (double)(a / b);
+			break;
+		}
+		printf("Invalid action\n");
+		break;
+	default:
+		printf("Invalid action");
 		break;
 
-	case '-': sum = a - b;
-		printf("Result is %lf\n", sum);
-		break;
-
-	case '*': sum = a * b;
-		printf("Result is %lf\n", sum);
-		break;
-
-	case '/': sum = a / b;
-		printf("Result is %lf\n", sum);
-	default: printf("Invalid result\n");
-	break;
 	}
-
+	printf("The result is %lf\n", result);
 }
