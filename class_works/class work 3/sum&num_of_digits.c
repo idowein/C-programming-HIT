e22610c157c9e75b6753c37ab4b836b351d9893f
idowein <ido.weinstock@gmail.com>
2024-12-11 16:20:31 +0200

    adjusting count int

--- class_works/class work 3/sum&num_of_digits.c
+++ class_works/class work 3/sum&num_of_digits.c
@@ -1,23 +1,23 @@
 # include <stdio.h>
-#define _CRT_SECURE_NO_WARNINGS
+# define _CRT_SECURE_NO_WARNINGS
 
 void main() {
+
 	// calculating the sum of the digits in a number and how many digits in it
 
-	int input, dig, count = 0, sum = 0;
+	int input, dig, count = -1, sum = 0;
 	printf("Enter a num\n");
 	scanf_s("%d", &input);
 
 	do {
 
 		dig = input % 10;
-		sum += dig;
+		sum += dig; // calc digit's sum
 		count++;
 		input /= 10;
 
 	} while (dig!=0 || input > 0);
 
-	count --;
 	printf("The sum is %d and the count is %d", sum, count);
 
 }
