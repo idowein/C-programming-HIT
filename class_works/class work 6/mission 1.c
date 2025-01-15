5ac654ac511a4537da4899811f46aebe55dfa862
idowein <ido.weinstock@gmail.com>
2025-01-15 18:00:11 +0200

    start to coding mission1 using pointers

--- class_works/class work 6/mission 1.c
+++ class_works/class work 6/mission 1.c
@@ -5,36 +5,40 @@
 #include <math.h>
 
 /* Function declarations */
-
+int arr_creating();
+void under_average_printing();
+void above_average_printing();
 /* Declarations of other sub-functions */
-
+int value_QA();
 /* ------------------------------- */
 
 int main()
 {
-	int grades_arr[6];
+	int grades_arr[5] ;
+	int grade_avg = arr_creating(&grades_arr[5]);
+}
+
+/* Function definitions */
+
+int arr_creating(int *grades_arr[]) {
 	int i = 0;
-	double avg = 0;
+	int avg = 0;
 	printf("Please enter your grades.\n");
 	do {
 		scanf_s("%d", &grades_arr[i]);
-		while (grades_arr[i] < 0 || grades_arr[i]>100) {
+		while (&grades_arr[i] < 0 || &grades_arr[i]>100) {
 			printf("Please enter valid grade\n");
 			scanf_s("%d", &grades_arr[i]);
 		}
 	} while (i < 6);
 
 	for (int i = 0; i < 6; i++) {
-		avg += arr[i];
+		avg += *grades_arr[i];
 	}
 	avg /= 5;
-
-	printf("The avg is %lf\n", avg);
-
+	printf("The avg is %d\n", avg);
+	return avg;
 }
-
-/* Function definitions */
-
 
 /* Sub-function definitions */
 
